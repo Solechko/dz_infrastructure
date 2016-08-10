46c0643f1a4f1305e5dcb52be6ee138882e29bef
Solechko <yana.b617@yandex.ru>
2016-08-10 17:51:01 +0300

    Исправлено форматирование кода

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+    response.render('pages/index');
 });
 
 app.get('/cool', function(request, response) {
-  console.time('render');
-  response.send(cool());
-  console.timeEnd('render');
+    console.time('render');
+    response.send(cool());
+    console.timeEnd('render');
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
