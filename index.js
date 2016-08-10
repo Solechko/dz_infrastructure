acbb5cd5a2d698a6f6de4d5cabd0df4613a44c2a
Solechko <yana.b617@yandex.ru>
2016-08-10 16:40:24 +0300

    Добавлено логиирование времени на рендеринг

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ app.get('/', function(request, response) {
 });
 
 app.get('/cool', function(request, response) {
+  console.time('render');
   response.send(cool());
+  console.timeEnd('render');
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
